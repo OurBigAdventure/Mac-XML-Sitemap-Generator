f8a897eb1f24e02e120b1af1ae4abaafc4a89b8e
Chris Brown <chris@OurBigAdventure.com>
2013-02-08 16:24:05 -0500

    Added View Controller for UI Layout

---
+++ Sitemap/OBASitemapViewController.h
@@ -0,0 +1,13 @@
+//
+//  OBASitemapViewController.h
+//  Sitemap
+//
+//  Created by Chris Brown on 2/8/13.
+//  Copyright (c) 2013 Chris Brown. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface OBASitemapViewController : NSViewController
+
+@end
---
+++ Sitemap/OBASitemapViewController.m
@@ -0,0 +1,27 @@
+//
+//  OBASitemapViewController.m
+//  Sitemap
+//
+//  Created by Chris Brown on 2/8/13.
+//  Copyright (c) 2013 Chris Brown. All rights reserved.
+//
+
+#import "OBASitemapViewController.h"
+
+@interface OBASitemapViewController ()
+
+@end
+
+@implementation OBASitemapViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
