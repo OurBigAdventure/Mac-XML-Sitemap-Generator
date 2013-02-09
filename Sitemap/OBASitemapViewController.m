37d12b54a0ae9523a616300618e36bc36dc16cd0
Chris Brown <chris@OurBigAdventure.com>
2013-02-09 14:38:11 -0500

    Added Data to Table Connections

--- Sitemap/OBASitemapViewController.m
+++ Sitemap/OBASitemapViewController.m
@@ -24,4 +24,30 @@
     return self;
 }
 
+- (NSView *)tableView:(NSTableView *)tableView viewForTableColumn:(NSTableColumn *)tableColumn row:(NSInteger)row {
+    
+    // Get a new ViewCell
+    NSTableCellView *cellView = [tableView makeViewWithIdentifier:tableColumn.identifier owner:self];
+    
+    // Depending on the column, return the value that matches the cell's value
+    if ([tableColumn.identifier isEqualToString:@"URLColumn"]) {
+        OBASitemapData *data = [self.URLs objectAtIndex:row];
+        cellView.textField.stringValue = data.URL;
+        return cellView;
+    } else if ([tableColumn.identifier isEqualToString:@"InboundLinks"]) {
+        OBASitemapData *data = [self.URLs objectAtIndex:row];
+        cellView.textField.stringValue = [NSString stringWithFormat:@"%ld", (long)data.numLinks];
+        return cellView;
+    } else if ([tableColumn.identifier isEqualToString:@"CalculatedPriority"]) {
+        OBASitemapData *data = [self.URLs objectAtIndex:row];
+        cellView.textField.stringValue = [NSString stringWithFormat:@"%0.1f", data.rating];
+        return cellView;
+    }
+    return cellView;
+}
+
+- (NSInteger)numberOfRowsInTableView:(NSTableView *)tableView {
+    return [self.URLs count];
+}
+
 @end
