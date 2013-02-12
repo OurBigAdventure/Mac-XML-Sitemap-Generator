c828d6ede0448b058dbeaf6af939ab2cef1e9214
Chris Brown <chris@OurBigAdventure.com>
2013-02-12 15:21:16 -0500

    Removed Sample Test Data

--- Sitemap/OBAAppDelegate.m
+++ Sitemap/OBAAppDelegate.m
@@ -21,13 +21,13 @@
     self.sitemapViewController = [[OBASitemapViewController alloc] initWithNibName:@"OBASitemapViewController" bundle:nil];
     
     // Sample Data
-    OBASitemapData *data1 = [[OBASitemapData alloc] initWithURL:@"http://www.ourbigadventure.com" rating:0.3 numLinks:4];
-    OBASitemapData *data2 = [[OBASitemapData alloc] initWithURL:@"http://www.ourbigadventure.com/category/At%20Home" rating:0.5 numLinks:2];
-    OBASitemapData *data3 = [[OBASitemapData alloc] initWithURL:@"http://www.ourbigadventure.com/category/holidays" rating:0.1 numLinks:2];
-    OBASitemapData *data4 = [[OBASitemapData alloc] initWithURL:@"http://www.ourbigadventure.com/category/vacations" rating:0.7 numLinks:7];
-    NSMutableArray *URLs = [NSMutableArray arrayWithObjects:data1, data2, data3, data4, nil];
-    
-    self.sitemapViewController.URLs = URLs;
+//    OBASitemapData *data1 = [[OBASitemapData alloc] initWithURL:@"http://www.ourbigadventure.com" rating:0.3 numLinks:4];
+//    OBASitemapData *data2 = [[OBASitemapData alloc] initWithURL:@"http://www.ourbigadventure.com/category/At%20Home" rating:0.5 numLinks:2];
+//    OBASitemapData *data3 = [[OBASitemapData alloc] initWithURL:@"http://www.ourbigadventure.com/category/holidays" rating:0.1 numLinks:2];
+//    OBASitemapData *data4 = [[OBASitemapData alloc] initWithURL:@"http://www.ourbigadventure.com/category/vacations" rating:0.7 numLinks:7];
+//    NSMutableArray *URLs = [NSMutableArray arrayWithObjects:data1, data2, data3, data4, nil];
+//    
+//    self.sitemapViewController.URLs = URLs;
     
     // Add the view controller to the window's content view
     [self.window.contentView addSubview:self.sitemapViewController.view];
